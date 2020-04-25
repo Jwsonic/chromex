9cb84892d8606ce2c4eb053288035ade92fcf976
John Palgut <jwsonic@gmail.com>
2020-04-25 10:19:48 -0700

    initial commit

---
+++ lib/chromex.ex
@@ -0,0 +1,18 @@
+defmodule Chromex do
+  @moduledoc """
+  Documentation for `Chromex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chromex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
