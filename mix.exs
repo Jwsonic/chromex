a36835ae5ebe6886072ad31d44fbd8e2b3c3dc8f
John Palgut <jwsonic@gmail.com>
2020-04-27 22:23:26 -0700

    Remove Browser starting at boot

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule Chromex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Chromex.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
